94d2fd4a5829842327980ebe2e336869c3372a00
Vicsonlee <lee_vicson@hotmail.com>
2017-10-14 01:30:45 +0800

    TRAINWRECK 2 COMING TO CINEMAS NEAR YOU

--- project_1b/45_layer.py
+++ project_1b/45_layer.py
@@ -1,6 +1,7 @@
 """Project 1b: Approximation."""
 
 import time
+import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -77,32 +78,45 @@ def main(train_x, train_y, test_x, test_y, no_hidden=30, learning_rate=1e-4):
     no_samples = T.scalar('no_samples')
 
     # initialize weights and biases for hidden layer(s) and output layer
-    w_o = theano.shared(np.random.randn(no_hidden) * .01, floatX)
+    w_o = theano.shared(np.random.randn(20) * .01, floatX)
     b_o = theano.shared(np.random.randn() * .01, floatX)
     w_h1 = theano.shared(np.random.randn(
         no_features, no_hidden) * .01, floatX)
     b_h1 = theano.shared(np.random.randn(no_hidden) * 0.01, floatX)
+    w_h2 = theano.shared(np.random.randn(
+        no_hidden, 20) * .01, floatX)
+    b_h2 = theano.shared(np.random.randn(20) * 0.01, floatX)
+    w_h3 = theano.shared(np.random.randn(
+        20, 20) * .01, floatX)
+    b_h3 = theano.shared(np.random.randn(20) * 0.01, floatX)
 
     # learning rate
     alpha = theano.shared(learning_rate, floatX)
 
     # Define mathematical expression:
     h1_out = T.nnet.sigmoid(T.dot(x_mat, w_h1) + b_h1)
-    y_vec = T.dot(h1_out, w_o) + b_o
+    h2_out = T.nnet.sigmoid(T.dot(h1_out, w_h2) + b_h2)
+    h3_out = T.nnet.sigmoid(T.dot(h2_out, w_h3) + b_h3)
+    y_vec = T.dot(h3_out, w_o) + b_o
 
     cost = T.abs_(T.mean(T.sqr(d_mat - y_vec)))
     accuracy = T.mean(d_mat - y_vec)
 
     # define gradients
-    dw_o, db_o, dw_h, db_h = T.grad(cost, [w_o, b_o, w_h1, b_h1])
+    dw_o, db_o, dw_h1, db_h1, dw_h2, db_h2, dw_h3, db_h3 = \
+          T.grad(cost, [w_o, b_o, w_h1, b_h1, w_h2, b_h2, w_h3, b_h3])
 
     train = theano.function(
         inputs=[x_mat, d_mat],
         outputs=cost,
         updates=[[w_o, w_o - alpha * dw_o],
                  [b_o, b_o - alpha * db_o],
-                 [w_h1, w_h1 - alpha * dw_h],
-                 [b_h1, b_h1 - alpha * db_h]],
+                 [w_h1, w_h1 - alpha * dw_h1],
+                 [b_h1, b_h1 - alpha * db_h1],
+                 [w_h2, w_h2 - alpha * dw_h2],
+                 [b_h2, b_h2 - alpha * db_h2],
+                 [w_h3, w_h3 - alpha * dw_h3],
+                 [b_h3, b_h3 - alpha * db_h3]],
         allow_input_downcast=True
     )
 
@@ -118,13 +132,18 @@ def main(train_x, train_y, test_x, test_y, no_hidden=30, learning_rate=1e-4):
 
     min_error = 1e+15
     best_iter = 0
-    best_w_o = np.zeros(no_hidden)
+    best_w_o = np.zeros(20)
     best_w_h1 = np.zeros([no_features, no_hidden])
+    best_w_h2 = np.zeros([no_hidden, 20])
+    best_w_h3 = np.zeros([20, 20])
+    
     best_b_o = 0
     best_b_h1 = np.zeros(no_hidden)
+    best_b_h2 = np.zeros(20)
+    best_b_h3 = np.zeros(20)
 
     alpha.set_value(learning_rate)
-    print(alpha.get_value(), no_hidden)
+    print(alpha.get_value())
 
     t = time.time()
     for iter in tqdm(range(epochs)):
@@ -139,14 +158,23 @@ def main(train_x, train_y, test_x, test_y, no_hidden=30, learning_rate=1e-4):
             min_error = test_cost[iter]
             best_w_o = w_o.get_value()
             best_w_h1 = w_h1.get_value()
+            best_w_h2 = w_h2.get_value()
+            best_w_h3 = w_h3.get_value()
+            
             best_b_o = b_o.get_value()
             best_b_h1 = b_h1.get_value()
+            best_b_h2 = b_h2.get_value()
+            best_b_h3 = b_h3.get_value()
 
     # set weights and biases to values at which performance was best
     w_o.set_value(best_w_o)
     b_o.set_value(best_b_o)
     w_h1.set_value(best_w_h1)
     b_h1.set_value(best_b_h1)
+    w_h2.set_value(best_w_h2)
+    b_h2.set_value(best_b_h2)
+    w_h3.set_value(best_w_h3)
+    b_h3.set_value(best_b_h3)
 
     best_pred, best_cost, best_accuracy = test(test_x, np.transpose(test_y))
 
@@ -180,28 +208,21 @@ if __name__ == '__main__':
     validation_args = []
     
     train_x, train_y, test_x, test_y = load_train_test() 
-    for no_hidden in search_space:
-        train_cost, test_cost, _ = main(train_x, train_y, test_x, test_y, no_hidden=no_hidden)
-        #train_cost, test_cost = cross_validation(train_x, train_y, no_hidden=no_hidden)
-        train_args += [train_cost]
-        validation_args += [test_cost]
-
+##    for no_hidden in search_space:
+##        train_cost, test_cost, _ = main(train_x, train_y, test_x, test_y, no_hidden=no_hidden)
+##        #train_cost, test_cost = cross_validation(train_x, train_y, no_hidden=no_hidden)
+##        train_args += [train_cost]
+##        validation_args += [test_cost]
+    train_cost, test_cost, _ = main(train_x, train_y, test_x, test_y, no_hidden=60)
+    with open("5_layer.pkl",'wb+') as saveFile:
+        pickle.dump(test_cost, saveFile)
+    
     # Plots
     plt.figure()
-    for item, value in zip(train_args, search_space):
-        plt.plot(range(epochs), item, label="neurons={}".format(value))
-    plt.xlabel('Epochs')
-    plt.ylabel('Mean Squared Error')
-    plt.title('Training Error')
-    plt.legend()
-    #plt.savefig('p1b3_neuron_train.png')
-
-    plt.figure()
-    for item, value in zip(validation_args, search_space):
-        plt.plot(range(epochs), item, label="neurons={}".format(value))
+    plt.plot(range(epochs), test_cost, label="5 layer")
     plt.xlabel('Epochs')
     plt.ylabel('Mean Squared Error')
     plt.title('Test Error')
     plt.legend()
-    plt.savefig('p1b3_neuron_test.png')
+    plt.savefig('p1b4_5layer_test.png')
     plt.show()
